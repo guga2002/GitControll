6013f5390cbabaac8074018ceb42e31d9aa84db1
Guga Apkhazava <gapkhazava@qarva.com>
2026-02-22 18:49:13 +0400

    add test controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitControll.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+
+    [HttpGet("TestIt")]
+    public IActionResult Test()
+    {
+        return Ok("Test successful!");
+    }
+}
